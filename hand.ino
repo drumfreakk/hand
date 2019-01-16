d7083964377010a02333116e4f33a1ec0dc72a1d
Kieran H <kieran.houtgraaf@gmail.com>
2019-01-16 20:04:00 +0100

    Added explanations

--- hand.ino
+++ hand.ino
@@ -1,18 +1,18 @@
 #include <Servo.h>
 
-int maxTurn = 150;
+int maxTurn = 150;  // het aanatl graden dat het draait, dit is ongeveer het max met deze servos
 
 struct Vinger{
   int posi = 0;
   Servo serv;
 };
 
-Vinger vinger[5];
+Vinger vinger[5];   // het aantal servos dat je gebruikt
 
 void setup() {
   Serial.begin(9600);
 
-  int pin[5] = {2, 3, 4, 5, 6};
+  int pin[5] = {2, 3, 4, 5, 6};   // de pinnen, als je minder dan vijf servos gebruikt gebruikt het de eerste pinnen
   for(int x = 0; x < 5; x++){
     vinger[x].serv.attach(pin[x]);
   }
@@ -22,13 +22,13 @@ void setup() {
 void loop() {
   // turn all fingers forward and back one by one
   for(auto &toturn : vinger){   // each finger
-    toturn.posi = 0;            // set the position to 0;
+  //  toturn.posi = 0;            // set the position to 0;
     turnFor(toturn, maxTurn);   // turn to maxTurn
     
-    toturn.posi = maxTurn;      // set the position to maxTurn
+  //  toturn.posi = maxTurn;      // set the position to maxTurn
     turnBack(toturn, 0);  // turn to 0
     
-    toturn.posi = 0;            // set the position to 0
+  //  toturn.posi = 0;            // set the position to 0
   }
 
   // turn all fingers forward
@@ -39,7 +39,7 @@ void loop() {
   }
 
   for(auto &toturn: vinger){             // each finger
-    toturn.posi = maxTurn;               // set the position to max
+  //  toturn.posi = maxTurn;               // set the position to max
   }
 
   // turn all fingers back
